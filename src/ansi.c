c920a4841bbd06d02cd7a0e7c5c5c5711f4ead63
Aidan T. Manning <periodicaidan@gmail.com>
2020-09-30 14:15:58 -0500

    changed tabs to spaces

--- src/ansi.c
+++ src/ansi.c
@@ -2,11 +2,11 @@
 
 const char* ansiUnderline(const char* s)
 {
-	char* underlined;
-	size_t underlinedLen = strlen(s) + UNDERLINE_ESCSEQ_LEN;
-	underlined = (char*) malloc(underlinedLen * sizeof(char));
+    char* underlined;
+    size_t underlinedLen = strlen(s) + UNDERLINE_ESCSEQ_LEN;
+    underlined = (char*) malloc(underlinedLen * sizeof(char));
 
-	//                 don't forget the NUL!
-	snprintf(underlined, underlinedLen + 1, UNDERLINE_ESCSEQ, s);
-	return underlined;
+    //                 don't forget the NUL!
+    snprintf(underlined, underlinedLen + 1, UNDERLINE_ESCSEQ, s);
+    return underlined;
 }
--- src/main.c
+++ src/main.c
@@ -1,11 +1,11 @@
 // Throw a compiler error on a Windows machine that doesn't have Cygwin or MinGW
 #if IS_WINDOWS
-	#error "winsz uses the UNIX API, and will not work on Windows without Cygwin or MinGW"
+    #error "winsz uses the UNIX API, and will not work on Windows without Cygwin or MinGW"
 #endif
 
 #include <stdio.h>
-#include <unistd.h>		// For STDIN_FILENO
-#include <sys/ioctl.h>	// for ioctl and TIOCGWINSZ
+#include <unistd.h>     // For STDIN_FILENO
+#include <sys/ioctl.h>  // for ioctl and TIOCGWINSZ
 
 #include "ansi.h"
 
@@ -13,15 +13,16 @@
 #define getWinSize(ws) (ioctl(STDIN_FILENO, TIOCGWINSZ, &ws))
 
 #define IS_WINDOWS \
-	(defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !(defined(__CYGWIN__) || defined(__MINGW32__)))
+    (defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !(defined(__CYGWIN__) || defined(__MINGW32__)))
 
 int main(void)
 {
-	struct winsize ws;
-	if (getWinSize(ws) == 0) {
-		puts(ansiUnderline("Rows\tCols"));
-		printf("%d\t%d\n", ws.ws_row, ws.ws_col);
-	} else {
-		return EXIT_FAILURE;
-	}
+    struct winsize ws;
+    if (getWinSize(ws) == 0) {
+        // FIXME: I think this causes a memory leak lol
+        puts(ansiUnderline("Rows\tCols"));
+        printf("%d\t%d\n", ws.ws_row, ws.ws_col);
+    } else {
+        return EXIT_FAILURE;
+    }
 }
